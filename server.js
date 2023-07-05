d23a19b110df68da8b0bc3ffe42a344fb65151b3
= <=>
2023-07-05 11:06:09 +0700

    upadate-02

--- server.js
+++ server.js
@@ -25,6 +25,6 @@ app.get("/api/config/paypal", (req, res) => {
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 1000;
+const PORT = process.env.PORT || 9001;
 
 app.listen(PORT, console.log(`server run in port ${PORT}`));
