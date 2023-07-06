57e3ef0fed364d2754d81faa7d60324a161a64aa
= <=>
2023-07-06 10:04:17 +0700

    update cors express

--- server.js
+++ server.js
@@ -15,6 +15,13 @@ connectDatabase();
 const app = express();
 app.use(express.json());
 app.use(cors())
+app.use(function (req, res, next) {
+  //Enabling CORS
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+    next();
+  });
 
 // app.use(cors({
 //   origin: '*'
