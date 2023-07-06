8bd3c6915175cbc35cc8bdf337a4b8633e048b6f
= <=>
2023-07-06 10:08:54 +0700

    update allowed

--- server.js
+++ server.js
@@ -14,7 +14,16 @@ dotenv.config();
 connectDatabase();
 const app = express();
 app.use(express.json());
-app.use(cors())
+
+
+const cors = require('cors');
+const corsOptions ={
+    origin:'https://shopcaycanh.vercel.app', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
+
 app.use(function (req, res, next) {
   //Enabling CORS
   res.header("Access-Control-Allow-Origin", "*");
